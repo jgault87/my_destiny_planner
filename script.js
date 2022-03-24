759264f9491601db50f5c6c4ef594e20ab34a944
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-03-23 23:18:41 -0600

    UI tables and beginning time functions

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+//display current time and date elements moment.js
+var currentDay = moment().format("dddd LL, [<br>] h:mm a");
+$("#currentDay").append(currentDay);
+
+
+
+
+var checkTime = function () {
+
+    var currentHour = moment().format('h');
+  console.log(currentHour);
+
+    var timeBlocks = $(".textarea");
+  
+    // loop through all text area timeblocks
+    for (var i = 0 ; i < timeBlocks.length ; i++) {
+
+    
+     
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
