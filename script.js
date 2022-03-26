e707bc2927e941009d5b0067597a968171098a97
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-03-25 21:33:28 -0600

    bugfixed code for saving to local storage not targeting the right parent

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //call functions once dom fully loaded
 
 $(function () {
-  // colorizeTimeBlocks();
-  
+  updateColor();
+  setTime();
 });
 
 //display current time and date elements moment.js
@@ -10,136 +10,58 @@ var currentDay = moment().format("dddd LL, [<br>] h:mm a");
 // display moment format to page header
 $("#currentDay").append(currentDay);
 
-// returns number of current hour for timeblock comparison
-var now = new Date().getHours();
+function updateColor() {
+  var now = new Date().getHours(); // returns number of current hour
 
-// console.log(now); //test time catch
+  //function to change colors of timeblocks based on var now
+  $(".hourblock").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    // console.log(this);
 
-//function to change colors of timeblocks based on var now result
+    //target and change DOM el based on var now number and blockHour number
+    let el = "#block" + blockHour;
+    if (now > blockHour) {
+      $(el).addClass("past");
+    } else if (now >= blockHour && now < blockHour + 1) {
+      $(el).addClass("present");
+    } else if (now < blockHour) {
+      $(el).addClass("future");
+    }
 
-// switch(now)  {
-// case now > 9 :
-// $("#block9am").addClass("past");
-// break;
+    console.log(el);
+  });
+}
 
-// }
- 
+//function to refresh updateColor() every 5 mins
+updateColor();
+var secondsLeft = 60 * 5;
+function setTime() {
+  setInterval(function () {
+    secondsLeft--;
 
-$('.hourblock').each(function () {
-  var blockHour = parseInt($(this).attr('id').split('-')[1]);
-// console.log(this);
+    if (secondsLeft === 0) {
+      updateColor();
+      secondsLeft = 60 * 5;
+    }
+  }, 1000);
+}
 
-  // now = 15
-  // blockHour = 9
-  let el = ('#block' + blockHour)
- //  `The hour is ${blockHour}`
-  // $(el).addClass("present")
-  if (now > blockHour) {
-    $(el).addClass("past");
-  } else if (now >= blockHour && now < blockHour+1) {
-    $(el).addClass("present");
-  } else if (now < blockHour) {
-    $(el).addClass("future");
-  }
-
-console.log(el);
-
-})
-
-
-// function colorizeTimeBlocks() {
-//   if (now > 9) {
-//     $("#block9am").addClass("past");
-//   } else if (now >= 9 && now < 10) {
-//     $("#block9am").addClass("present");
-//   } else if (now < 9) {
-//     $("#block9am").addClass("future");
-//   }
-
-//   if (now > 10) {
-//     $("#block10am").addClass("past");
-//   } else if (now >= 10 && now < 11) {
-//     $("#block10am").addClass("present");
-//   } else if (now < 10) {
-//     $("#block10am").addClass("future");
-//   }
-
-//   if (now > 11) {
-//     $("#block11am").addClass("past");
-//   } else if (now >= 11 && now < 12) {
-//     $("#block11am").addClass("present");
-//   } else if (now < 11) {
-//     $("#block11am").addClass("future");
-//   }
-
-//   if (now > 12) {
-//     $("#block12pm").addClass("past");
-//   } else if (now >= 12 && now < 13) {
-//     $("#block12pm").addClass("present");
-//   } else if (now < 12) {
-//     $("#block12pm").addClass("future");
-//   }
-
-//   if (now > 13) {
-//     $("#block1pm").addClass("past");
-//   } else if (now >= 13 && now < 14) {
-//     $("#block1pm").addClass("present");
-//   } else if (now < 13) {
-//     $("#block1pm").addClass("future");
-//   }
-
-//   if (now > 14) {
-//     $("#block2pm").addClass("past");
-//   } else if (now >= 14 && now < 15) {
-//     $("#block2pm").addClass("present");
-//   } else if (now < 14) {
-//     $("#block2pm").addClass("future");
-//   }
-
-//   if (now > 15) {
-//     $("#block3pm").addClass("past");
-//   } else if (now >= 15 && now < 16) {
-//     $("#block3pm").addClass("present");
-//   } else if (now < 15) {
-//     $("#block3pm").addClass("future");
-//   }
-
-//   if (now > 16) {
-//     $("#block4pm").addClass("past");
-//   } else if (now >= 16 && now < 17) {
-//     $("#block4pm").addClass("present");
-//   } else if (now < 16) {
-//     $("#block4pm").addClass("future");
-//   }
-
-//   if (now > 17) {
-//     $("#block5pm").addClass("past");
-//   } else if (now >= 17 && now < 18) {
-//     $("#block5pm").addClass("present");
-//   } else if (now < 17) {
-//     $("#block5pm").addClass("future");
-//   }
-// }
-
-
-var textInput = $("<textarea>");
-
-
-//save button on click function
 $(".saveBtn").on("click", function () {
-  
-console.log("saveBtn");
+  //target local storage elements
+  var value = $(this).parent().siblings(".description").val();
+  var time = $(this).parent().parent().attr("id");
 
-
+  // save in localStorage
+  localStorage.setItem(time, value);
 });
 
-
-
-function init() {
-  // getworkEvents();
-console.log("INITIALIZED");
-
-  }
-
-// var workEvents = JSON.parse(localStorage.getItem("workDay"));
-
+// load any saved data from localStorage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
